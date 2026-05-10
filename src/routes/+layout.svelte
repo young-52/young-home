c3b88508bc074bd76e925fe2872c5d6dcbdf50de
Park Junyoung <bloomwayz@snu.ac.kr>
2026-05-10 14:29:15 +0900

    🎨 Format Svelte codes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,15 @@ let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<title>Junyoung Park | language, computer, and people</title>
-	<meta name="description" content="Junyoung Park | language, computer, and people" />
+  <link rel="icon" href={favicon} />
+  <title>Junyoung Park | language, computer, and people</title>
+  <meta
+    name="description"
+    content="Junyoung Park | language, computer, and people"
+  />
 </svelte:head>
 <div class="flex min-h-svh w-full flex-col bg-[#f8f8f6] px-4 sm:px-6 lg:px-8">
-	<main class="flex flex-1 flex-col">
-		{@render children()}
-	</main>
+  <main class="flex flex-1 flex-col">
+    {@render children()}
+  </main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,52 @@
 <script>
+import profileImg from '$lib/assets/profile.jpg'
 import { Button } from '$lib/components/ui/button/index.js'
 </script>
 
+<div class="flex flex-1 w-full justify-center">
+  <div class="flex items-center gap-8">
+    <img src={profileImg} alt="Junyoung Park" class="rounded-full size-40" />
+    <div class="flex flex-col justify-center gap-6">
+      <div class="flex flex-col gap-3 justify-center">
+        <span class="name-primary"><strong>PARK</strong> Junyoung</span>
+        <span class="name-secondary">
+          <strong class="font-semibold">/박준영/</strong> [pɐk̚ tɕu.nʲʌ̹ŋ]
+        </span>
+      </div>
+      <div class="flex gap-1 items-center">
+        <span class="body-base">Studying Linguistics & Computer Science at</span
+        >
+        <Button
+          variant="link"
+          href="https://snu.ac.kr"
+          class="text-[18px] font-normal px-0"
+        >
+          SNU
+        </Button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <style>
-	.name-primary {
-		font-family: "Hedvig Letters Serif", serif;
+  .name-primary {
+    font-family: "Hedvig Letters Serif", serif;
     font-size: 36px;
     font-style: normal;
     line-height: 120%;
   }
 
   .name-secondary {
-		font-size: 18px;
-		font-style: normal;
-		line-height: 120%;
+    font-size: 18px;
+    font-style: normal;
+    line-height: 120%;
   }
 
   .body-base {
-		color: #79716b;
-		font-size: 16px;
-		font-style: normal;
-		font-weight: 400;
-		line-height: 120%;
+    color: #79716b;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 120%;
   }
 </style>
-
-<div class="flex flex-1 w-full justify-center">
-	<div class="flex items-center gap-8">
-	<img src="src/lib/assets/profile.jpg" alt="Junyoung Park" class="rounded-full size-40" />
-	<div class="flex flex-col justify-center gap-6">
-		<div class="flex flex-col gap-3 justify-center">
-			<span class="name-primary"><strong>PARK</strong> Junyoung</span>
-			<span class="name-secondary"><strong class="font-semibold">/박준영/</strong> [pɐk̚ tɕu.nʲʌ̹ŋ]</span>
-		</div>
-			<div class="flex gap-1 items-center">
-				<span class="body-base">Studying Linguistics & Computer Science at</span>
-				<Button variant="link" href="https://snu.ac.kr" class="text-[18px] font-normal px-0">SNU</Button>
-			</div>
-	</div>
-</div>
-</div>
